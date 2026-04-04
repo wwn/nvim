13d21405165fcdfce55025d35d13a6a2ca20680c
wwn <wwn@mailbox.org>
2026-04-04 12:54:44 +0200

    init

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("config")
+
+if vim.g.neovide then
+    vim.o.guifont = "JetBrainsMono Nerd Font Mono:h16"
+    vim.g.neovide_cursor_vfx_mode = "railgun"	
+end
+
---
+++ lua/config/options.lua
@@ -0,0 +1,27 @@
+local opt = vim.opt
+
+vim.cmd("colorscheme delek")
+
+opt.mouse = "a"               -- allows mouse actions
+opt.clipboard = "unnamedplus" -- synchc clipboard
+
+
+opt.number = true             -- line numbers
+opt.relativenumber = true
+opt.termguicolors = true
+opt.cursorline = true         -- marks current line
+opt.scrolloff = 8             -- 8 lines gap to top and bottom
+
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true          -- don't ignore case when enter upper case
+
+
+-- indentation
+opt.tabstop = 4               -- tab width
+opt.shiftwidth = 4            -- indent width
+opt.softtabstop = 2           -- soft tab stop
+opt.expandtab = true          -- use spaces instead of tabs
+opt.smartindent = true        -- smart auto-indenting
+opt.autoindent = true         -- copy indent from current line
