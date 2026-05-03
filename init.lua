15c5952aee3ac9def94b38780eb0a09ef2a179ed
wwn <wwn@mailbox.org>
2026-05-03 12:53:42 +0200

    no ligatures anymore

--- init.lua
+++ init.lua
@@ -15,14 +15,15 @@ vim.pack.add {
 require("user.main") -- require("config") would search for lua/config/init.lua
 
 if vim.g.neovide then
-    local os_name = vim.uv.os_uname().sysname
+    vim.g.neovide_font_features = "-calt, -liga, -dlig, -clig"
 
+    local os_name = vim.uv.os_uname().sysname
     if os_name == "Darwin" then
-        vim.o.guifont = "JetBrainsMono Nerd Font Mono:h16"
+        vim.o.guifont = "JetBrainsMonoNL Nerd Font Mono:h16"
     elseif os_name == "Windows_NT" then
-        vim.o.guifont = "JetBrainsMono_Nerd_Font_Mono:h12"
+        vim.o.guifont = "JetBrainsMonoNL Nerd Font Mono:h12"
     else
-        vim.o.guifont = "JetBrainsMono Nerd Font Mono 12" --maybe for fedora
+        vim.o.guifont = "JetBrainsMonoNL Nerd Font Mono:h12"
     end
 
     vim.g.neovide_cursor_vfx_mode = "railgun"
